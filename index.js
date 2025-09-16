6dd8464c229f84c0dfd5d22f75e1462f19f6cbde
Nyala Jackson <nymjackson@gmail.com>
2025-09-16 16:00:58 -0400

    Break after class

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
-const navBar = document.getElementById("top-nav")
+const navBar = document.getElementById("top-nav");
 const navMenuLinks = ["New Game", "Top Scores"];
-const board = document.getElementById("board")
+const board = document.getElementById("board");
 
-
-function createNavBar(){
-    const ul = document.createElement("ul")
-    ul.classList.add("nav-bar")
-    for(let i = 0; i < navMenuLinks.length; i++){
-        const li = document.createElement("li")
-        li.classList.add("nav-link")
-        li.textContent = navMenuLinks[i]
-        ul.appendChild(li)
-    }
-    navBar.appendChild(ul)
+function createNavBar() {
+  const ul = document.createElement("ul");
+  ul.classList.add("nav-bar");
+  for (let i = 0; i < navMenuLinks.length; i++) {
+    const li = document.createElement("li");
+    li.classList.add("nav-link");
+    li.textContent = navMenuLinks[i];
+    ul.appendChild(li);
+  }
+  navBar.appendChild(ul);
 }
 
-function createTicTacBox(){
-    const fragment = document.createDocumentFragment()
-    for(let i = 0; i < 3;i++){
-        const row = document.createElement("div")
-        row.classList.add("row")
-        for(let j = 0; j < 3; j++){
-            const box = document.createElement("div")
-            box.classList.add("box")
-            row.appendChild(box)
-        }
-        fragment.appendChild(row)
+function createTicTacBox() {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < 3; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 3; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      row.appendChild(box);
     }
-    board.appendChild(fragment)
+    fragment.appendChild(row);
+  }
+  board.appendChild(fragment);
 }
 
-createNavBar()
-createTicTacBox()
+createNavBar();
+createTicTacBox();
+
+{
+  //in progress, in {} so not to break ^
+}
+function placeSymbol(e) {
+  const ticO = document.createElement("i");
+  ticO.classList.add("fa-solid","fa-o");
+  const target = e.target
+  console.log(target)
+  target.appendChild(ticO)
+}
+
+board.addEventListener("click", placeSymbol);
