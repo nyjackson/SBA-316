ae370a31234129c84525854677bd187663caf065
Nyala Jackson <nymjackson@gmail.com>
2025-09-17 18:17:01 -0400

    Switching Gears Again: Post/Commenting Page

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 ## Resource Links: ##
 [Coolors.co](https://coolors.co/0d1f22-264027-3c5233-f1bf98-b38a58)
+[Assignment Link](https://perscholas.instructure.com/courses/2875/assignments/556714)
 ## Files: ##
 
 ## Functions: Definitions & Usage ##
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const navBar = document.getElementById("top-nav");
 const app = document.getElementById("app")
-const navMenuLinks = ["New Quiz", "Quiz Results"];
-const questions = ["lorem", "ipsum?"];
-const answers = ["lorem", "ipsum?"];
+const commentList = document.getElementById("comment-list")
+const navMenuLinks = ["Account", "Settings"];
+
 function createNavBar() {
   const ul = document.createElement("ul");
   ul.classList.add("nav-bar");
@@ -16,38 +16,9 @@ function createNavBar() {
 }
 createNavBar();
 
-function createQuestion(){ 
+function createComment(){
   const frag = document.createDocumentFragment()
-  const div = document.createElement("div")
-  const question = document.createElement("h2")
-  question.textContent = q;
-  div.appendChild(question)
-  const choices = document.createElement("form")
-  for(let i = 0; i < answers.length; i++){
-    const container = document.createElement("div")
-    const choice = document.createElement("input")
-    const label = document.createElement("label")
-    choice.setAttribute("type", "radio")
-    choice.setAttribute("name",i)
-    label.setAttribute("for", i)
-    label.textContent = answers[i]
-    container.appendChild(choice)
-    container.appendChild(label)
-
-    //choice.classList.add("q-answer")
-    choices.appendChild(container)
-  }
-  frag.appendChild(div)
-  app.appendChild(frag)
-  //return frag
-  // const template = document.getElementsByTagName("template")[0]
-  // const clone = template.content.cloneNode(true)
-  // app.appendChild(clone)
-}
-
-createQuestion()
-
-function buildQuiz(){
-  const form = document.createElement("form")
-  return
+  const commentBox = document.createElement("div")
+  //const comment = document.createElement("input")
+  commentList.prependChild(frag)
 }
