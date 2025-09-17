759555a677349ddabb9ca93b10538666840c7e62
Nyala Jackson <nymjackson@gmail.com>
2025-09-16 21:02:49 -0400

    working on question logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const navBar = document.getElementById("top-nav");
+const app = document.getElementById("app")
 const navMenuLinks = ["New Quiz", "Quiz Results"];
-
+const questions = ["lorem", "ipsum?"];
+const answers = ["lorem", "ipsum?"];
 function createNavBar() {
   const ul = document.createElement("ul");
   ul.classList.add("nav-bar");
@@ -14,13 +16,27 @@ function createNavBar() {
 }
 createNavBar();
 
-{
-  //in progress, in {} so not to break ^
+function createQuestion(q, answers){ 
+  const frag = document.createDocumentFragment()
+  const div = document.createElement("div")
+  const question = document.createElement("h2")
+  question.textContent = q;
+  div.appendChild(question)
+  const choices = document.createElement("form")
+  for(let i = 0; i < answers.length; i++){
+    const choice = document.createElement("input")
+    choice.setAttribute("type", "radio")
+    //choice.classList.add("q-answer")
+    choices.appendChild(choice)
+  }
+  frag.appendChild(div)
+  app.appendChild(frag)
+  //return frag
 }
-function placeSymbol(e) {
-  const ticO = document.createElement("i");
-  ticO.classList.add("fa-solid","fa-o");
-  const target = e.target
-  console.log(target)
-  target.appendChild(ticO)
+
+createQuestion("Random?",answers)
+
+function buildQuiz(){
+  const form = document.createElement("form")
+  return
 }
