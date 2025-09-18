a251b60d1ddc15713fe852a51bd743ad42fec02d
Nyala Jackson <nymjackson@gmail.com>
2025-09-17 21:38:48 -0400

    Changed radio buttons to form, added submit button and created counters to be added to the og-post

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const navBar = document.getElementById("top-nav");
 const app = document.getElementById("app")
 const commentList = document.getElementById("comment-list")
 const commentButton = document.getElementById("og-comment")
+const form = document.querySelector("form")
 const navMenuLinks = ["Account", "Settings"];
+let likeCounter = 0
+let dislikeCounter = 0
 
 function createNavBar() {
   const ul = document.createElement("ul");
@@ -20,14 +23,33 @@ createNavBar();
 
 function createComment(){
   const frag = document.createDocumentFragment()
-  let user = window.prompt("Enter your name:")
   let commentResponse = window.prompt("Enter your comment:")
+  let userResponse = window.prompt("Enter a name:")
   console.log(commentResponse)
   const comment = document.createElement("div")
   comment.textContent = commentResponse
   comment.classList.add("comment")
+  comment.setAttribute("maxLength",120)
   frag.appendChild(comment)
+  
+  const p = document.createElement("p")
+  p.textContent = "- " + userResponse
+  comment.appendChild(p)
   commentList.prepend(frag)
 }
 
+function addLikeOrDislike(e){
+e.preventDefault()
+let choices = form.querySelectorAll("input")
+console.log(choices)
+for(let i = 0; i < choices.length;i++){
+  console.log(choices[i])
+  if(choices[i].checked == true){
+    choices[i].value == "like" ? likeCounter++ : dislikeCounter++
+    window.alert(`Your ${choices[i].value} vote has been submitted!`)
+  }
+
+}
+}
 commentButton.addEventListener("click", createComment)
+form.addEventListener("submit", addLikeOrDislike)
